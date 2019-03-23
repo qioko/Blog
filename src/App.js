33355701df8269b0e0972407eb2c45aea49f5a6c
Luo <cco.oul@gmail.com>
2019-03-24 03:34:09 +0800

    remove yarn

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import BeginAnimation from './BeginAnimation';
+import Entry from './Entry';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <BeginAnimation />
+        <Entry/>
+        {/* <BeginAnimation /> */}
       </div>
     );
   }
---
+++ src/Entry/index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './index.css';
+
+export default class Entry extends Component {
+  render() {
+    return (
+      <div className="main">
+      </div>
+    );
+  }
+}
