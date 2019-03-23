06e93458b47aeec2e97749236970ff3c7d2442a1
queti <queti@qunhemail.com>
2019-03-23 21:17:17 +0800

    add fireworks

---
+++ src/BeginAnimation/Chapter/Fireworks/index.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './index.css';
+
+export default class BeginAnimation extends Component {
+  firework = [1,1,1,1,1,1,1,1] //TODO 扩展角度数量
+  render() {
+    return (
+      <div className="sky">
+        <div className="firework type_1">
+          {this.firework.map(i=><div className="fire"></div>)}
+        </div>
+        <div className="firework type_2">
+          {this.firework.map(i=><div className="fire"></div>)}
+        </div>
+        <div className="firework type_3">
+          {this.firework.map(i=><div className="fire"></div>)}
+        </div>
+      </div>
+    );
+  }
+}
---
+++ src/BeginAnimation/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './index.css';
+import Fireworks from './Chapter/Fireworks'
+
+export default class BeginAnimation extends Component {
+  render() {
+    return (
+      <div className="main">
+        <Fireworks/>
+      </div>
+    );
+  }
+}
