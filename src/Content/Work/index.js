dc914bf13519814da6ac9a61e43ca39ea61090a1
Luo <cco.oul@gmail.com>
2019-03-25 16:07:57 +0800

    change url

--- src/Content/Work/index.js
+++ src/Content/Work/index.js
@@ -10,13 +10,13 @@ const work = [
         name:"Canvas Clock",
         type:"word",
         description:"一个使用Canvas绘制时钟的项目",
-        link:"https://github.com/qioko/react-jianshu",
+        link:"https://qioko.github.io/canvas-clock/",
         imgUrl:"http://pov9a1cu3.bkt.clouddn.com/clock.png"
     },
     {
         name:"简历",
         type:"image",
-        contentUrl:"http://pov9a1cu3.bkt.clouddn.com/test_resume.jpg",
+        contentUrl:"http://pov9a1cu3.bkt.clouddn.com/frontend_resume.jpg",
         imgUrl:"http://pov9a1cu3.bkt.clouddn.com/resume-clip.png"
     }
 ]
